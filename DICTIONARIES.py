d65d32aa855ec0ee996baf283fb4fb319ec2ffd4
keremclp <kerem00962@gmail.com>
2022-07-25 23:49:42 +0300

    DICTIONARIES

---
+++ DICTIONARIES.py
@@ -0,0 +1,16 @@
+monthsconversions ={
+    "Jan": "January",
+    "Feb": "February",
+    "Mar": "March",
+    "Apr": "April",         #coding train ,   the new boston,      brad traversy
+    "May": "May",
+    "Jun": "June",
+    "Jul": "July",
+    "Aug": "August",
+    "Sep": "September",
+    "Oct": "October",
+    "Nov": "November",
+    "Dec": "December",
+}
+
+print(monthsconversions["Aug"])  # print(monthconversion.get("Dec"))
